7086791f485821ba5df8a8997bd1ab925ba12ec1
Sniper Boy <quangthai30062002@gmail.com>
2024-08-23 15:58:04 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import configViewEngine from "./configs/viewEngine"
+import initWebRouter from "./routes/web"
+require("dotenv").config()
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+
+//config viewEngine
+configViewEngine(app)
+
+// init web routes
+initWebRouter(app)
+
+app.listen(PORT, () => {
+    console.log(">>> JWT Backend is running on the port = "+ PORT)
+})
